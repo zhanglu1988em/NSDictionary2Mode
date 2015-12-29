375628297df40c54a94bca987463b139090db393
zhanglu1988em <zhanglu1988em@163.com>
2015-12-29 14:23:54 +0800

    Initial Commit

---
+++ KvcTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KvcTestDemo
+//
+//  Created by wbder on 15/12/29.
+//  Copyright © 2015年 wbder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
